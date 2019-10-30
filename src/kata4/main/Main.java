6d28adab1ec0b43a866c2fd1e6d351ae992ba167
Usuario <Usuario@10.22.148.96>
2019-10-30 10:02:47 +0000

    Versión final Kata4

--- src/kata4/main/Main.java
+++ src/kata4/main/Main.java
@@ -1,26 +1,23 @@
 package kata4.main;
 
+import java.util.List;
 import kata4.view.HistogramDisplay;
 import kata4.model.Histogram;
+import kata4.model.Mail;
+import kata4.view.MailHistogramBuilder;
+import kata4.view.MailListReader;
 
 public class Main {
     
     public static void main(String[] args) {
-
-        Histogram<String> histogram = new Histogram<>();
         
-        histogram.increment("gmail.com");
-        histogram.increment("ulpgc.es");
-        histogram.increment("ulpgc.es");
-        histogram.increment("dis.ulpgc.es");
-        histogram.increment("ulpgc.es");
-        histogram.increment("gmail.com");
-        histogram.increment("dis.ulpgc.es");
-        histogram.increment("gmail.com");
-        
-        
-        HistogramDisplay histoDisplay = new HistogramDisplay(histogram);
-        histoDisplay.execute();
+        String fileName = new String("email.txt");
+        List<Mail> mailList = MailListReader.read(fileName);            /* I */
+                
+        Histogram histogram = MailHistogramBuilder.build(mailList);     /* P */
+               
+        HistogramDisplay histoDisplay = new HistogramDisplay(histogram);/* O */
+        histoDisplay.execute();                                         /* O */
         
     }
     
